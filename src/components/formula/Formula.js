1861d94874bdb48dfb7daea929641824be4e96ec
Alice <andreevaalisa42@gmail.com>
2022-07-22 18:06:42 +0300

    add dynamic markup

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,5 +1,12 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Formula extends ExcelComponent {
+  static className = 'excel__formula'; // jshint ignore:line
 
+  toHTML() {
+    return `
+    <div class="formula-info">fx</div>
+        <div class="formula-input" contenteditable spellcheck="false"></div>
+    `;
+  }
 }
--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,2 +1,8 @@
 export class DOMListener {
+  constructor($root) {
+    if (!$root) {
+      throw new Error('No $root was provided to DomListener');
+    }
+    this.$root = $root;
+  }
 }
