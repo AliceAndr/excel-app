70e0a81ae17fbc93db494095bd9a58ab31870e53
Alice <andreevaalisa42@gmail.com>
2022-07-25 21:00:47 +0300

    finish resize markup and styles

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,12 @@ import {createTable} from '@/components/table/table.template';
 export class Table extends ExcelComponent {
   static className = 'excel__table'; // jshint ignore:line
 
+  constructor($root) {
+    super($root, {
+      listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    });
+  }
+
   toHTML() {
     return createTable();
   }
