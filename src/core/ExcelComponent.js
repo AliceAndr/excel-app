abd6f11c574fbef69b8fd0c73b71f9d4d0fecbb7
Alice <andreevaalisa42@gmail.com>
2022-07-27 14:40:34 +0300

    add selection multiple cells

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,7 +4,14 @@ export class ExcelComponent extends DOMListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name;
+
+    this.prepare();
   }
+
+  prepare() {
+
+  }
+
   // Возвращает шаблон компонента
   toHTML() {
     return '';
